d4576e05fa41a7e746b6cc7124caadb7f76f6c23
Tejashri04 <114688886+Tejashri04@users.noreply.github.com>
2024-03-02 13:56:11 +0530

    Add files via upload

---
+++ inbuilt_desc.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class inbuilt_desc{
+
+    public static void printArr(Integer arr[]){
+        for(Integer i = 0 ; i < arr.length ; i++){
+            System.out.print(arr[i] + "  ");
+        }
+    } 
+
+    public static void main(String args[]){
+        Integer arr[] = { 5,4,1,3,2};
+        
+        // Arrays.sort(arr,Collections.reverseOrder());
+        // printArr(arr); 
+
+        System.out.println();
+
+        Arrays.sort(arr,0,3,Collections.reverseOrder());
+        printArr(arr); 
+        
+        
+
+    }
+}
